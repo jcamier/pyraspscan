6fc5ba044ddb9e80c60a386beda1299c3b790232
James Vogel <jim.m.vogel@gmail.com>
2019-01-06 11:48:50 -0600

    added configuration via .env file

--- app.py
+++ app.py
@@ -2,44 +2,43 @@ import asyncio
 import serial_asyncio
 import aiohttp
 
-endpoint = 'http://localhost:8000/scan'
+from settings import config
 
 session = aiohttp.ClientSession()
 loop = asyncio.get_event_loop()
 
+
 async def post_data(data):
-    post_data = {'data':data.decode()}
-    async with session.post(endpoint, data=post_data) as resp:
-        print('hitting endpoint')
+    post_data = {
+        "data": data.decode().strip(),
+        "machine": config.machine_id,
+        "port": config.serial_port,
+    }
+    async with session.post(config.endpoint, data=post_data) as resp:
+        print("hitting endpoint")
         print(resp.status)
         print(await resp.text())
+
 
 class Output(asyncio.Protocol):
     def connection_made(self, transport):
         self.transport = transport
-        print('port opened', transport)
-        transport.serial.rts = False  # You can manipulate Serial object via transport
-        transport.write(b'Hello, World!\n')  # Write serial data via transport
+        print("port opened", transport)
 
     def data_received(self, data):
-        print('data received', repr(data))
+        print("data received", repr(data))
         loop.create_task(post_data(data))
-        if b'\n' in data:
+        if b"\n" in data:
             self.transport.close()
 
     def connection_lost(self, exc):
-        print('port closed')
+        print("port closed")
         self.transport.loop.stop()
 
-    def pause_writing(self):
-        print('pause writing')
-        print(self.transport.get_write_buffer_size())
 
-    def resume_writing(self):
-        print(self.transport.get_write_buffer_size())
-        print('resume writing')
-
-coro = serial_asyncio.create_serial_connection(loop, Output, '/dev/ttyACM0', baudrate=115200)
-loop.run_until_complete(coro)
+serial_monitor = serial_asyncio.create_serial_connection(
+    loop, Output, config.serial_port, baudrate=config.baud
+)
+loop.run_until_complete(serial_monitor)
 loop.run_forever()
 loop.close()
---
+++ settings.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from os import getenv
+from dotmap import DotMap
+
+config = DotMap()
+
+config.endpoint = getenv("ENDPOINT", "http://localhost:8000/scan")
+config.serial_port = getenv("SERIAL_PORT", "/dev/ttyACM0")
+config.baud = int(getenv("BAUD", "115200"))
+config.machine_id = getenv("MACHINE_ID", "dev_machine")
+
